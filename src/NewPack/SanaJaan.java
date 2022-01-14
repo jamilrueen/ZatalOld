730678624da0b87334e16c2ca21b967f282b543e
abduljamil <jamil.rueen58@gmail.com>
2022-01-14 14:45:15 -0500

    First Commit

---
+++ src/NewPack/SanaJaan.java
@@ -0,0 +1,17 @@
+package NewPack;
+
+public class SanaJaan {
+
+	public static void main(String[] args) {
+		String [][] names= { 
+				{"Jamil", "Malik"},
+				{"Liza", "Zarmina"}
+				};
+		for (int i=0; i<=1; i++) {
+			for (int j=0; j<=1; j++) {
+				System.out.println(names[i][j]);
+			}
+		}
+	}
+
+}
