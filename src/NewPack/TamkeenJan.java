1168c70b5a2f6f3a6c284394826753bc47ed5ed2
abduljamil <jamil.rueen58@gmail.com>
2022-01-14 15:00:52 -0500

    New Changes

---
+++ src/NewPack/TamkeenJan.java
@@ -0,0 +1,25 @@
+package NewPack;
+
+public class TamkeenJan {
+
+	public static void main(String[] args) {
+		
+		int day=21;
+		
+		switch (day) {
+		
+		case 1: 
+			System.out.println("Saturday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+			default:
+				System.out.println("Not Valid");
+				
+				
+		}
+
+	}
+
+}
