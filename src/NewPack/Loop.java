5df21d1832bac26e1e8f3712602b79f6ff9605ef
abduljamil <jamil.rueen58@gmail.com>
2022-01-15 15:46:26 -0500

    Loos Lesson

---
+++ src/NewPack/Loop.java
@@ -0,0 +1,18 @@
+package NewPack;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=4; i++){
+			System.out.println("Week"+i);
+			for (int j=1; j<=7; j++) {
+				System.out.println("Day"+j);
+			}
+			
+			
+		}
+
+	}
+
+}
