1492d147d782feb1d373e4a5374ea1782c9cd6ad
abduljamil <jamil.rueen58@gmail.com>
2022-01-15 15:37:30 -0500

    TDArry added

---
+++ src/NewPack/TDArray.java
@@ -0,0 +1,16 @@
+package NewPack;
+
+public class TDArray {
+
+	public static void main(String[] args) {
+		
+		String[][] names= {
+				{"Jamil", "Nadim"},
+				{"Safi", "Saifurrahman"}
+		};
+		
+		System.out.println(names[1][0]);
+
+	}
+
+}
